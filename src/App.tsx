911530ce178bd0f944291551d50ea47c83376891
[React2] 박찬호 <qkrcksgh5654@gmail.com>
2025-05-16 18:31:31 +0900

    영상섹션 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BasicInformationSection from "./components/BasicInformationSection";
 import CertificationEducation from "./components/CertificationEducation";
 import Footer from "./components/Footer";
 import Header from "./components/header/Header";
-import MainSection from "./components/MainSection";
+// import MainSection from "./components/MainSection";
 import ProjectSection from "./components/ProjectSection";
 import SkillSection from "./components/SkillSection";
 
